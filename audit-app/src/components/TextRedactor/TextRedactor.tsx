9760f265358e361d45e01d2aab439ddfaaf882e5
Egor <79940875+Bareen86@users.noreply.github.com>
2023-05-20 20:10:26 +0300

    add feature save as redactor content as PDF

--- audit-app/src/components/TextRedactor/TextRedactor.tsx
+++ audit-app/src/components/TextRedactor/TextRedactor.tsx
@@ -1,23 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ReactQuill, {Quill} from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import "./TextRedactor.css";
+import { saveAs } from 'file-saver';
+import { pdfExporter } from 'quill-to-pdf';
 
 function TextRedactor() {
-    const [value, setValue] = useState("");
+    const [content, setContent] = useState('');
+    const quillRef = useRef<ReactQuill>(null);
+
+    
+
+    const exportAsPDF = async () => {
+      const data:any = quillRef.current?.editor?.getContents(); // gets the Quill delta
+      const pdfAsBlob = await pdfExporter.generatePdf(data); // converts to PDF
+      saveAs(pdfAsBlob, "Audit.pdf"); // downloads from the browser
+    };
 
     return (
         <div className="textRedactor">
             <CustomToolbar />
             <ReactQuill
                 placeholder="Введите текст."
+                ref={quillRef}
+                value={content}
+                onChange={setContent}
                 modules={TextRedactor.modules}
                 formats={TextRedactor.formats}
             />
+            <button className="savePDF" onClick={exportAsPDF}>Сохранить в PDF</button>
         </div>
     );
 }
-
 
 const CustomToolbar = () => (
   <div id="toolbar">
@@ -32,14 +46,14 @@ const CustomToolbar = () => (
       <option value="lucida">Lucida</option>
     </select>
     <select className="ql-header">
-            <option value="1"></option>
+            <option value="1" selected></option>
             <option value="2"></option>
             <option value="3"></option>
             <option value="4"></option>
             <option selected></option>
         </select>
     <select className="ql-size">
-      <option value="extra-small">13</option>
+      <option value="extra-small" selected>13</option>
       <option value="small">14</option>
       <option value="medium" selected>
         18
@@ -47,6 +61,7 @@ const CustomToolbar = () => (
       <option value="large">20</option>
     </select>
     <select className="ql-align" />
+    <button className="ql-image"></button>
   </div>
 );
 
@@ -70,7 +85,6 @@ Quill.register(Font, true);
 TextRedactor.modules = {
   toolbar: {
     container: "#toolbar",
-
   }
 };
 
@@ -94,15 +108,6 @@ TextRedactor.formats = [
 
 
 export default TextRedactor;
-
-// const fontStyles = {
-//   roboto: {
-//     fontFamily: 'Roboto, sans-serif',
-//   },
-//   arial: {
-//     fontFamily: 'Arial, sans-serif',
-//   },
-// };
 
 
 

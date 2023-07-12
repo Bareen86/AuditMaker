1009bfc447b92bd2b2b9ff9928446e46c02cf463
Egor <79940875+Bareen86@users.noreply.github.com>
2023-07-12 15:14:51 +0300

    убрал неиспользуемые файлы

--- AuditApp/AuditApp.Api/Modules/Users/Dtos/LoginResultDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuditApp.Extranet.Modules.Users.Dtos
-{
-    public class LoginResultDto
-    {
-        public string Ligin { get; set; }
-        public string Password { get; set; } 
-    }
-}
